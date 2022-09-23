f1dd8c1ebbb6edd463538ccd995ac191d1aec43c
Anthony Pham <akpham95@gmail.com>
2022-09-23 11:28:52 -0500

    orders.js finished

--- orders.js
+++ orders.js
@@ -29,7 +29,18 @@
     stored on the object.
 */
 
-//CODE HERE
+class Ticket {
+    constructor(items, orderTime, customerId, status){
+        this.items = items;
+        this.orderTime = orderTime;
+        this.customerId = customerId;
+        this.status = 'queued';
+    }
+    updateStatus(newStatus){
+        this.status = newStatus
+        console.log(`The order for customer ${this.customerId} is now ${this.status}`)
+    }
+}
 
 
 
@@ -44,7 +55,8 @@
     customer: 575
 */
 
-//CODE HERE
+let firstTicket = new Ticket(['pizza','bread','soda'], '7:03PM', 575)
+console.log(firstTicket)
 
 
 /*
@@ -53,4 +65,5 @@
     'cooking'
 */
 
-//CODE HERE
+firstTicket.updateStatus('cooking')
+console.log(firstTicket)
